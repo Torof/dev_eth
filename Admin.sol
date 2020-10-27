234175185610437fb68b4c9ea4da43ab94bb38e2
Torof <72133413+Torof@users.noreply.github.com>
2020-10-27 22:16:33 +0100

    Update Admin.sol
    lighter version

--- Admin.sol
+++ Admin.sol
@@ -7,51 +7,51 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/
 pragma solidity >=0.6.11;
 
 contract Admin is Ownable {
-    mapping(address => bool)whitelist;
-    mapping(address => bool)blacklist;
+    mapping(address => bool)public whitelist;
+    mapping(address => bool)public blacklist;
 
     event Whitelisted(address _toWhiteList, string text );
     event Blacklisted(address _toBlacklist, string text );
-    // un seul événement réutilisable:
-    // event isListed(address _isListed, string text);
+/* un seul événement réutilisable:
+    event isListed(address _isListed, string text);  */
 
-    function whitelist(address _toWhiteList) public onlyOwner returns(bool){
+    function whitelist(address _toWhiteList) public onlyOwner {
         whitelist[_toWhiteList] = true;
-        emit Whitelisted(_toWhiteList, "is now white listed");
-        return true;
+        emit Whitelisted(_toWhiteList, "whitelisted");
     }
 
-    function blacklist (address _toBlacklist) public onlyOwner returns(bool){
+    function blacklist (address _toBlacklist) public onlyOwner{
         blacklist[_toBlacklist] = true;
-        emit Blacklisted(_toBlacklist, "is now blacklisted");
-        return true;
+        emit Blacklisted(_toBlacklist, "blacklisted");
     }
-
-    function isBlacklisted (address _toCheck) public returns(bool){
-        if(blacklist[_toCheck] == true){
-            emit Blacklisted(_toCheck, "is black listed");
-            return  true;
-        } else {
-            emit Blacklisted(_toCheck, "is not black listed");
-            return false ;
-        }
-    }
-
-    function isWhiteListed (address _toCheck) public returns(bool){
-        if(whitelist[_toCheck] == true){
-            emit Whitelisted(_toCheck, "is white listed");
-            return  true;
-        } else {
-            emit Whitelisted(_toCheck, "is not white listed");
-            return false ;
-        }
-    }
-
-    function removeFromBlackList (address _toRemove) public onlyOwner {
+    
+     function removeFromBlackList (address _toRemove) public onlyOwner {
         blacklist[_toRemove] = false; 
     }
 
     function removeFromWhiteList (address _toRemove) public onlyOwner {
         whitelist[_toRemove] = false;
     }
+
+/* @dev  IsBlacklisted and is Whitelisted not necessary  if mappings declared public */
+    function isBlacklisted (address _toCheck) public returns(bool){
+        if(blacklist[_toCheck] == true){
+            emit Blacklisted(_toCheck, "is blacklisted");
+            return  true;
+        } else {
+            emit Blacklisted(_toCheck, "not blacklisted");
+            return false ;
+        }
+    }
+
+    function isWhiteListed (address _toCheck) public returns(bool){
+        if(whitelist[_toCheck] == true){
+            emit Whitelisted(_toCheck, "is whitelisted");
+            return  true;
+        } else {
+            emit Whitelisted(_toCheck, "not whitelisted");
+            return false ;
+        }
+    }
+
 }
